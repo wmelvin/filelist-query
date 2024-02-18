4d9e9dab51cd50bc5ba1de91c9e10ef508398895
Bill Melvin <bill@billmelvin.com>
2024-02-17 19:20:34 -0500

    Add initial Textual widgets

--- README.md
+++ README.md
@@ -6,5 +6,6 @@ Textual user interface for querying databases created by filelist-tools.
 
 - [wmelvin/filelist-tools](https://github.com/wmelvin/filelist-tools)
 - [Textual](https://textual.textualize.io/)
+- [Just](https://github.com/casey/just)
 - [Click](https://palletsprojects.com/p/click/)
 - [Hatch](https://hatch.pypa.io/latest/)
--- src/filelist_query/ui.py
+++ src/filelist_query/ui.py
@@ -2,12 +2,43 @@ from __future__ import annotations
 
 from pathlib import Path
 
-from textual.app import App
+from textual.app import App, ComposeResult
+from textual.widgets import Footer, Header, Label, Static, TabbedContent, TabPane
 
 from filelist_query.data import get_db_file
 
 
+class FileTab(TabPane):
+    def compose(self) -> ComposeResult:
+        yield Static("File", classes="tab-content")
+
+class FieldsTab(TabPane):
+    def compose(self) -> ComposeResult:
+        yield Static("Fields", classes="tab-content")
+
+
+class CriteriaTab(TabPane):
+    def compose(self) -> ComposeResult:
+        yield Static("Criteria", classes="tab-content")
+
+
+class QueryTab(TabPane):
+    def compose(self) -> ComposeResult:
+        yield Static("Query", classes="tab-content")
+
+
+class ResultsTab(TabPane):
+    def compose(self) -> ComposeResult:
+        yield Label("Results", classes="tab-content", id="results-content")
+
+
 class UI(App):
+    BINDINGS = [
+        ("x", "exit_app", "eXit"),
+        ("d", "toggle_dark", "Toggle dark mode"),
+    ]
+    CSS_PATH = "style.tcss"
+
     def __init__(self, db_file: str | Path = None):
         if db_file is None:
             self._db_file = get_db_file()
@@ -17,6 +48,22 @@ class UI(App):
             self._db_file = db_file
         super().__init__()
 
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Footer()
+        with TabbedContent(initial="fields"):
+            yield FileTab("File", id="file")
+            yield FieldsTab("Fields", id="fields")
+            yield CriteriaTab("Criteria", id="criteria")
+            yield QueryTab("Query", id="query")
+            yield ResultsTab("Results", id="results")
+
+    def action_toggle_dark(self) -> None:
+        self.dark = not self.dark
+
+    def action_exit_app(self) -> None:
+        self.exit()
+
     @property
     def db_file(self) -> Path:
         return self._db_file
@@ -24,4 +71,5 @@ class UI(App):
 
 if __name__ == "__main__":
     ui = UI()
-    print(f"\n{ui.db_file = }")
+    # print(f"\n{ui.db_file = }")
+    ui.run()
